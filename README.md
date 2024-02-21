e869c49ab894a82b30fd8cb2b252b38105138302
Marius Liviu Briscut <marius.briscut5@gmail.com>
2024-02-21 12:33:43 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# movies_by_year
+
+A short program I wrote in Python to help me sort my list of movies to watch.
+
+My list was formatted but unordered, the format of my list was:
+
+```text
+Movie A (Release Year)
+Movie B (Release Year)
+Movie C (Release Year)
+.
+.
+.
+```
+
+I wanted to sort my list of movies by release year, ascending.
+
+So I came up with the following steps:
+
+1. Open the .txt file.
+2. Transform the data into a tuple containing all movies but the title and release year as two separate elements.
+3. Sort the movies using the *sorted* function and the lambda function as the key to sort by release year.
+4. Print the sorted tuple to the console for testing purposes.
+5. Save the tuple inside a new .txt file in the original format.
+6. Run the code.
---
+++ main.py
@@ -0,0 +1,37 @@
+def read_movies_from_file(filename):
+    """Reads movie data from a file and returns a list of tuples."""
+    movies = []
+    with open(filename, "r") as file:
+        for line in file:
+            name, year = line.strip().split("(")[:2]
+            year = year.strip()[:-1]
+            movies.append((name, year))
+    return movies
+
+
+def sort_movies_by_year(movies):
+    """Sorts movies by year in ascending order."""
+    return sorted(movies, reverse=False, key=lambda movie: movie[1])
+   
+   
+def print_movies(movies):
+    """Prints the list of movies to the console."""
+    if movies:
+        print(movies)
+    else:
+        print("File is empty!")
+
+
+def save_movies_to_file(movies, filename):
+    """Saves movies to a file in the specified format."""
+    with open(filename, "w") as output:
+        for name, year in movies:
+            output.write(f"{name} ({year})\n")
+           
+            
+"""Executing the program."""
+if __name__ == '__main__':
+    movies = read_movies_from_file("movies_to_watch.txt")
+    sorted_movies = sort_movies_by_year(movies)
+    print_movies(sorted_movies)
+    save_movies_to_file(sorted_movies, "movies_to_watch_by_year.txt")
